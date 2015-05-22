447a7a0eb2988c0007833d373d6be5f296e0e577
Howard Cordray <hcordray218@gmail.com>
2015-05-22 18:03:20 -0400

    Initial Commit

---
+++ ZillowExercise/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZillowExercise
+//
+//  Created by Howard Cordray on 5/22/15.
+//  Copyright (c) 2015 Howard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
